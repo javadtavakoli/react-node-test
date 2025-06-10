abcae3ee601c4957b572c1135b1e57c2cd318a0c
Javad Tavakoli <javadtavakoli95@gmail.com>
2025-06-10 22:15:45 +0330

    feat: employee modal validation

--- client/src/Pages/Users/CreateEmployee.jsx
+++ client/src/Pages/Users/CreateEmployee.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createEmployee } from "../../redux/action/user";
-import { useNavigate } from "react-router-dom";
-import Topbar from "./Topbar";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { employeeSchema } from "../../utils/validationSchemas";
 import {
-  Divider,
   Dialog,
   DialogContent,
   DialogTitle,
   Slide,
   DialogActions,
   TextField,
-  FormGroup,
-  FormControlLabel,
-  Checkbox,
 } from "@mui/material";
 import { PiNotepad, PiXLight } from "react-icons/pi";
-import { CFormSelect } from "@coreui/react";
-import { pakistanCities } from "../../constant";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="down" ref={ref} {...props} />;
@@ -27,39 +22,37 @@ const CreateUser = ({ open, setOpen, scroll }) => {
   //////////////////////////////////////// VARIABLES /////////////////////////////////////
   const { isFetching } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const initialEmployeeState = {
-    firstName: "",
-    lastName: "",
-    username: "",
-    password: "",
-    phone: "",
-    email: "",
-  }
 
-  //////////////////////////////////////// STATES /////////////////////////////////////
-  const [employeeData, setEmployeeData] = useState(initialEmployeeState);
-
-  //////////////////////////////////////// USE EFFECTS /////////////////////////////////////
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(employeeSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      phone: "",
+      email: "",
+    },
+  });
 
   //////////////////////////////////////// FUNCTIONS /////////////////////////////////////
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const { firstName, lastName, username, password, phone, email } = employeeData
-    if (!firstName || !lastName || !username || !password || !phone  )
-      return alert("Make sure to provide all the fields")
-    dispatch(createEmployee(employeeData, setOpen));
-    setEmployeeData(initialEmployeeState)
-  };
 
-  const handleChange = (field, value) => {
-    setEmployeeData((prevFilters) => ({ ...prevFilters, [field]: value, }));
-  };
 
   const handleClose = () => {
     setOpen(false);
-    setEmployeeData(initialEmployeeState)
+    reset();
   };
-
+  
+  const onSubmit = (data) => {
+    dispatch(createEmployee(data, setOpen));
+    handleClose();
+    reset();
+  };
   return (
     <div>
       <Dialog
@@ -71,110 +64,102 @@ const CreateUser = ({ open, setOpen, scroll }) => {
         fullWidth="sm"
         maxWidth="sm"
         aria-describedby="alert-dialog-slide-description">
-        <DialogTitle className="flex items-center justify-between">
-          <div className="text-sky-400 font-primary">Add New Employee</div>
-          <div className="cursor-pointer" onClick={handleClose}>
-            <PiXLight className="text-[25px]" />
-          </div>
-        </DialogTitle>
-        <DialogContent>
-          <div className="flex flex-col gap-2 p-3 text-gray-500 font-primary">
-            <div className="text-xl flex justify-start items-center gap-2 font-normal">
-              <PiNotepad size={23} />
-              <span>Employee Detials</span>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogTitle className="flex items-center justify-between">
+            <div className="text-sky-400 font-primary">Add New Employee</div>
+            <div className="cursor-pointer" onClick={handleClose}>
+              <PiXLight className="text-[25px]" />
             </div>
-            <Divider />
-            <table className="mt-4">
-              <tr>
-                <td className="pb-4 text-lg">First Name </td>
-                <td className="pb-4">
+          </DialogTitle>
+          <DialogContent>
+            <div className="flex flex-col gap-2 p-3 text-gray-500 font-primary">
+              <div className="text-xl flex justify-start items-center gap-2 font-normal">
+                <PiNotepad size={23} />
+                <span>Employee Details</span>
+              </div>
+              <div className="mt-4 space-y-4">
+                <div>
                   <TextField
                     size="small"
                     fullWidth
-                    value={employeeData.firstName}
-                    onChange={(e) => handleChange('firstName', e.target.value)}
+                    label="First Name"
+                    {...register("firstName")}
+                    error={!!errors.firstName}
+                    helperText={errors.firstName?.message}
                   />
-                </td>
-              </tr>
-              <tr>
-                <td className="pb-4 text-lg">Last Name </td>
-                <td className="pb-4">
+                </div>
+                <div>
                   <TextField
                     size="small"
                     fullWidth
-                    value={employeeData.lastName}
-                    onChange={(e) => handleChange('lastName', e.target.value)}
+                    label="Last Name"
+                    {...register("lastName")}
+                    error={!!errors.lastName}
+                    helperText={errors.lastName?.message}
                   />
-                </td>
-              </tr>
-              <tr>
-                <td className="pb-4 text-lg">User Name </td>
-                <td className="pb-4">
+                </div>
+                <div>
                   <TextField
                     size="small"
                     fullWidth
-                    value={employeeData.username}
-                    onChange={(e) => handleChange('username', e.target.value)}
+                    label="Username"
+                    {...register("username")}
+                    error={!!errors.username}
+                    helperText={errors.username?.message}
                   />
-                </td>
-              </tr>
-              <tr>
-                <td className="pb-4 text-lg">Email </td>
-                <td className="pb-4">
+                </div>
+                <div>
                   <TextField
                     size="small"
                     fullWidth
+                    label="Email"
                     placeholder="Optional"
-                    value={employeeData.email}
-                    onChange={(e) => handleChange('email', e.target.value)}
+                    {...register("email")}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
                   />
-                </td>
-              </tr>
-              <tr>
-                <td className="flex items-start pt-2 text-lg">Password </td>
-                <td className="pb-4">
+                </div>
+                <div>
                   <TextField
                     type="password"
-                    value={employeeData.password}
-                    onChange={(e) => handleChange("password", e.target.value)}
                     size="small"
                     fullWidth
+                    label="Password"
+                    {...register("password")}
+                    error={!!errors.password}
+                    helperText={errors.password?.message}
                   />
-                </td>
-              </tr>
-              <tr>
-                <td className="flex items-start pt-2 text-lg">Phone </td>
-                <td className="pb-4">
+                </div>
+                <div>
                   <TextField
                     type="number"
                     size="small"
-                    value={employeeData.phone}
-                    onChange={(e) => handleChange("phone", e.target.value)}
                     fullWidth
+                    label="Phone"
+                    {...register("phone")}
+                    error={!!errors.phone}
+                    helperText={errors.phone?.message}
                   />
-                </td>
-              </tr>
-            </table>
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <button
-            onClick={handleClose}
-            variant="contained"
-            type="reset"
-            className="bg-[#d7d7d7] px-4 py-2 rounded-lg text-gray-500 mt-4 hover:text-white hover:bg-[#6c757d] border-[2px] border-[#efeeee] hover:border-[#d7d7d7] font-thin transition-all">
-            Cancel
-          </button>
-          <button
-            onClick={handleSubmit}
-            variant="contained"
-            className="bg-primary-red px-4 py-2 rounded-lg text-white mt-4 hover:bg-red-400 font-thin">
-            {isFetching ? 'Submitting...' : 'Submit'}
-          </button>
-        </DialogActions>
+                </div>
+              </div>
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <button
+              onClick={handleClose}
+              type="button"
+              className="bg-[#d7d7d7] px-4 py-2 rounded-lg text-gray-500 mt-4 hover:text-white hover:bg-[#6c757d] border-[2px] border-[#efeeee] hover:border-[#d7d7d7] font-thin transition-all">
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-primary-red px-4 py-2 rounded-lg text-white mt-4 hover:bg-red-400 font-thin">
+              {isFetching ? 'Submitting...' : 'Submit'}
+            </button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
-
   );
 };
 
